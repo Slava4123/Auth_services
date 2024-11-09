f8bc9fa3b17bfe83ecda106bcaac66ac4542f74c
Slava Kovalchuk <Slava24122003@MAIL.RU>
2024-11-09 12:27:20 +0300

    Update db_connection

--- app/database/db_connection.py
+++ app/database/db_connection.py
@@ -1,4 +1,4 @@
-from decouple import config
+import os
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
 import loguru
@@ -6,11 +6,11 @@ import loguru
 from dotenv import load_dotenv
 
 load_dotenv()
-DB_NAME = config('DB_NAME')
-DB_USER = config('DB_USER')
-DB_PASSWORD = config('DB_PASSWORD')
-DB_HOST = config('DB_HOST')
-DB_PORT = config('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
